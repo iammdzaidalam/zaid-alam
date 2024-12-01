b80c7c4b95ecebf599157144177bc064519bc9fa
Zaid <161572905+iammdzaidalam@users.noreply.github.com>
2024-12-01 17:01:51 +0530

    refactor: Add lazy loading for images in App component
    Refactor the App component in src/App.jsx to improve page load performance by implementing lazy loading for images. This change includes importing the FadeInWhenVisible component from './components/FadeIn' and updating the sections array to include the FadeInWhenVisible component for each section.

--- src/App.jsx
+++ src/App.jsx
@@ -10,38 +10,41 @@ import Work from './components/Work';
 import { AnimatePresence } from 'framer-motion';
 import Preloader from './components/Preloader';
 import CodeforcesProfile from './components/CodeforcesProfile';
+import FadeInWhenVisible from './components/FadeIn';
+
+const sections = [
+  { id: 'about', component: <About /> },
+  { id: 'projects', component: <Projects /> },
+  { id: 'work', component: <Work /> },
+  { id: 'codeforces', component: <CodeforcesProfile /> },
+  { id: 'contact', component: <Contact /> },
+];
 
 const App = () => {
   const [isLoading, setIsLoading] = useState(true);
 
-  useEffect( () => {
-    (
-      async () => {
-          const LocomotiveScroll = (await import('locomotive-scroll')).default
-          const locomotiveScroll = new LocomotiveScroll();
+  useEffect(() => {
+    (async () => {
+      const LocomotiveScroll = (await import('locomotive-scroll')).default;
+      const locomotiveScroll = new LocomotiveScroll();
 
-          setTimeout( () => {
-            setIsLoading(false);
-            document.body.style.cursor = 'default'
-            window.scrollTo(0,0);
-          }, 2000)
-      }
-    )()
-  }, [])
+      setTimeout(() => {
+        setIsLoading(false);
+        document.body.style.cursor = 'default';
+        window.scrollTo(0, 0);
+      }, 2000);
+    })();
+  }, []);
 
   return (
     <main className="font-light text-customText antialiased selection:bg-customBgSel selection:text-customText2 h-auto min-h-0">
-      <AnimatePresence mode="wait">
-        {isLoading && <Preloader />}
-      </AnimatePresence>
-        <Navbar />
-        <Hero />
-        <Marquee />
-        <About />
-        <Projects />
-        <Work />
-        <CodeforcesProfile/>
-        <Contact />
+      <AnimatePresence mode="wait">{isLoading && <Preloader />}</AnimatePresence>
+      <Navbar />
+      <Hero />
+      <Marquee />
+      {sections.map((section) => (
+        <FadeInWhenVisible key={section.id}>{section.component}</FadeInWhenVisible>
+      ))}
     </main>
   );
 };
