c39bfe6ddf8ace096ab4c18e6ad0cb40526a0b95
Zaid <161572905+iammdzaidalam@users.noreply.github.com>
2024-12-03 16:43:01 +0530

    refactor: Improve image loading in Marquee component
    Refactor the Marquee component in src/components/Marquee.jsx to improve image loading. Changes include removing unnecessary state variables, simplifying the scroll direction logic, and optimizing the animation transitions. This change improves the performance and readability of the code.

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -1,52 +1,25 @@
-import { MARQUEE_TEXT } from "../constants";
-import { motion } from "framer-motion";
-import { useEffect, useState } from "react";
-
+import { MARQUEE_TEXT } from "../constants"
+import { motion } from "framer-motion"
 const Marquee = () => {
-  const [scrollDirection, setScrollDirection] = useState("left");
-  const [lastScrollTop, setLastScrollTop] = useState(0);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const currentScrollTop = window.scrollY;
-      if (currentScrollTop > lastScrollTop) {
-        // Scrolling downwards
-        setScrollDirection("right");
-      } else {
-        // Scrolling upwards
-        setScrollDirection("left");
-      }
-      setLastScrollTop(currentScrollTop <= 0 ? 0 : currentScrollTop); 
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, [lastScrollTop]);
-
   return (
-    <div className="mt-4 w-full bg-customText text-customText2 lg:py-6 overflow-hidden">
-      <motion.div
-        className="flex whitespace-nowrap"
-        initial={{ x: scrollDirection === "left" ? "0%" : "-100%" }}
-        animate={{ x: scrollDirection === "left" ? "-100%" : "0%" }}
-        transition={{
-          duration: 10, 
-          ease: "linear",
-          repeat: Infinity,
-        }}
-      >
-        
-        {[...Array(3)].map((_, index) => (
-          <span
-            key={index}
-            className="py-2 text-3xl font-bold leading-none tracking-tighter lg:text-7xl mx-4"
-          >
+    <div className="mt-4 w-full bg-customText text-customText2 lg:py-6">
+      <div className="flex overflow-hidden whitespace-nowrap">
+        {[...Array(2)].map((_, index) => (
+          <motion.h1
+          initial={{ x: "-100%" }}
+          animate={{ x: "0" }}
+            transition={{
+                duration: 20,
+                repeat: Infinity,
+                ease: "linear",
+            }}
+          key={index} className="py-2 text-3xl font-bold leading-none tracking-tighter lg:text-7xl">
             {MARQUEE_TEXT}
-          </span>
+          </motion.h1>
         ))}
-      </motion.div>
+      </div>
     </div>
-  );
-};
+  )
+}
 
-export default Marquee;
+export default Marquee;   
